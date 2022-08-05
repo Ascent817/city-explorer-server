75f086e6b727bf8a6c2c3100ca29ab7ddacf1303
= <vapradeep817@gmail.com>
2022-08-05 13:37:16 -0500

    Add cache

--- server.js
+++ server.js
@@ -7,14 +7,27 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+const cache = [];
 
 app.get('/weather', (request, response) => {
     if ('searchQuery' in request.query && 'lat' in request.query && 'lon' in request.query) {
         try {
             let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${request.query.lat}&lon=${request.query.lon}&appid=${process.env.WEATHER_API_KEY}`;
-            axios.get(url).then((forecast) => {
-                response.send(
-                    [{
+
+            let lat = request.query.lat;
+            let lon = request.query.lon;
+
+            if (`${lat}-${lon}` in cache) {
+                if (cache[`${lat}-${lon}`].time === new Date().toDateString()) {
+                    console.log('Used cache');
+                    response.send(cache[`${lat}-${lon}`].data);
+                } else {
+                    delete cache[`${lat}-${lon}`];
+                }
+            } else {
+                console.log('Used API')
+                axios.get(url).then((forecast) => {
+                    let responseData = [{
                         date: forecast.data.list[0].dt_txt.split(' ')[0],
                         highTemp: KelvinToFarenheit(forecast.data.list[0].main.temp_max),
                         lowTemp: KelvinToFarenheit(forecast.data.list[0].main.temp_min),
@@ -42,11 +55,16 @@ app.get('/weather', (request, response) => {
                         rain: forecast.data.list[32].pop,
                         description: forecast.data.list[32].weather[0].description
                     }]
-                );
-            }).catch((err) => {
-                console.log(err);
-                response.send({error: err});
-            });
+                    cache[`${lat}-${lon}`] = {
+                        data: responseData,
+                        time: new Date().toDateString()
+                    };
+                    response.send(responseData);
+                }).catch((err) => {
+                    console.log(err);
+                    response.send({error: err});
+                });
+            }
         } catch (error) {
             console.log(error);
             response.send({error: error});
